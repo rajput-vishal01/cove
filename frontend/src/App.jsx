a062e91751e6ac36dd0080f645e8a990e37571bf
rajput vishal <vishalrajput.7vr@gmail.com>
2026-04-05 11:58:13 +0530

    config:react-router-dom

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,19 @@
-import React from 'react'
-
+import { Routes, Route } from "react-router-dom";
+import Signup from "./pages/Signup";
+import Login from "./pages/Login";
+import Home from "./pages/Home";
+import Navbar from "./components/Navbar";
 const App = () => {
   return (
-    <div className=''>Cove</div>
-  )
-}
+    <>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+      </Routes>
+    </>
+  );
+};
 
-export default App
+export default App;
